443ac07a46ee3188552bc967d46af48f803cb562
Diego <diegom.gcalovi@gmail.com>
2021-09-01 07:39:38 -0400

    Adding doc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/**
+ * The functions converts a currency amount to a dolar.
+ */
 function converter () {
         const result = document.getElementById("inputAmount").value / document.getElementById("inputCurrency").value;
         document.getElementById("displayResult").innerText = `$${result.toFixed(2)}`;
